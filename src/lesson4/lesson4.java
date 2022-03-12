ddc0cc844ff209522535d100c1ca87ae82478042
Vasili Zlobin <vaszlbn@gmail.com>
2022-03-13 01:12:32 +0300

    add solution homework4

---
+++ src/lesson4/lesson4.java
@@ -0,0 +1,38 @@
+package lesson4;
+
+public class lesson4 {
+    private static final int SIZE = 3;
+    private static final int DOTS_FOR_WIN = 3;
+    private static final char DOT_EMPTY = '•';
+    private static final char DOT_X = 'X';
+    private static final char DOT_O = 'O';
+    private static char[][] map;
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    private static void printMap() {
+        for (int i = 1; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
