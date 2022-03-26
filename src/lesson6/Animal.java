107e52a734bbbaec4f722568d694d73c1f7a44da
Vasili Zlobin <vaszlbn@gmail.com>
2022-03-26 14:52:14 +0300

    add solution lesson6

---
+++ src/lesson6/Animal.java
@@ -0,0 +1,27 @@
+package lesson6;
+
+public class Animal {
+    private String name;
+    private Colors color;
+
+    public Animal(String name, Colors color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+}
---
+++ src/lesson6/Colors.java
@@ -0,0 +1,5 @@
+package lesson6;
+
+public enum Colors {
+    White, Black, Ginger, Spotted, Striped
+}
